343c14f4a1728d1fb3d7b654b44a0ac11a1b3f94
Miguel <sorth29@gmail.com>
2021-06-22 19:51:38 -0500

    se agrega la linea de requerir main

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(require('./src/routes/main'))
 
-app.get('/hola', (req, res) => {
+app.get('/', (req, res) => {
     res.send('mai eipiei');
 });
 
