0cb9ae0dfd6ef78415eae6518e45c76f687b0ba6
Miguel <sorth29@gmail.com>
2021-06-22 19:54:28 -0500

    se agrega la linea de requerir main

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('./src/config/database');
-const getConnection = require('./src/config/database');
+require('./src/config/database');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,74 +7,14 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.options('*', cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(require('./src/routes/main'))
+app.use(require('./src/routes/main'))
 
 app.get('/', (req, res) => {
     res.send('mai eipiei');
-});
-
-router.post('/login', (request, response) => {
-    connection = getConnection();
-    connection.connect( error => { 
-        if (error) {
-            console.log(error);
-            response.status(200).json({ error: true, status: 500, message: "Internal server error" });
-        }
-    });
-
-    const {
-        correo, 
-        pass
-    } = request.body;
-
-    const sql = `SELECT *  FROM usuarios
-    WHERE correo = ? AND pass = ?`;
-
-    const values = [ correo, pass ];
-
-    connection.query(sql, values, ( error, result ) => {
-        if (error) response.status(200).json({ error: true, status: 500, message: error.message });
-        if (result.length > 0) {
-            response.status(200).json({ error: false, status: 200, message: result[0] });
-        } else {
-            response.status(200).json({ error: true, status: 500, message: 'No result' });
-        }
-    });
-    connection.end();
-});
-
-
-
-router.post('/registro', (request, response) => {
-    connection = getConnection();
-    connection.connect( error => { 
-        if (error) {
-            console.log(error);
-            response.status(200).json({ error: true, status: 500, message: "Internal server error" });
-        }
-    });
-
-    const {
-        nombre, 
-        ap_paterno,
-        ap_materno,
-        correo,
-        pass,
-        tipo_sistema
-    } = request.body;
-
-    const sql = `INSERT INTO usuarios(nombre, ap_paterno, ap_materno, correo, pass, tipo_sistema) VALUES (?,?,?,?,?,?)`;
-
-    const values = [ nombre, ap_paterno, ap_materno, correo, pass, tipo_sistema];
-
-    connection.query(sql, values, ( error, result ) => {
-        if (error) response.status(200).json({ error: true, status: 500, message: error.message });
-        response.status(200).json({ error: false, status: 200, message: 'Usuario registrado' });
-    });
-    connection.end();
 });
 
 
